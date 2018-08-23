ac177e6b7741b783f4388b8e0c5120e57c323541
Lin <linxiaowei.me@gmail.com>
2018-08-23 09:30:47 +0800

    docs(uoo-web): 增加RouteController的注释

--- uoo-web/src/main/java/cn/ffcs/uoo/web/controller/RouteController.java
+++ uoo-web/src/main/java/cn/ffcs/uoo/web/controller/RouteController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * 测试Controller
+ *
+ * @author Lin
+ * @date 2018/08/23
+ */
 @Controller
 public class RouteController {
 
