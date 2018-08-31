a9576c23974a6d6bf3f4811aa073e8a934e1c673
WCNGS <WCNGS@QQ.COM>
2018-08-31 16:24:20 +0800

    添加通用类

---
+++ uoo-base/uoo-common/common-abstract/src/main/java/cn/ffcs/uoo/base/common/support/exception/ToolBoxException.java
@@ -0,0 +1,52 @@
+package cn.ffcs.uoo.base.common.support.exception;
+
+
+import cn.ffcs.uoo.base.common.support.StrKit;
+
+/**
+ *  ┏┓　　　┏┓
+ *┏┛┻━━━┛┻┓
+ *┃　　　　　　　┃ 　
+ *┃　　　━　　　┃
+ *┃　┳┛　┗┳　┃
+ *┃　　　　　　　┃
+ *┃　　　┻　　　┃
+ *┃　　　　　　　┃
+ *┗━┓　　　┏━┛
+ *　　┃　　　┃神兽保佑
+ *　　┃　　　┃代码无BUG！
+ *　　┃　　　┗━━━┓
+ *　　┃　　　　　　　┣┓
+ *　　┃　　　　　　　┏┛
+ *　　┗┓┓┏━┳┓┏┛
+ *　　　┃┫┫　┃┫┫
+ *　　　┗┻┛　┗┻┛
+ * @ClassName ToolBoxException
+ * @Description 工具类初始化异常
+ * @author WCNGS@QQ.COM
+ * @date 2018/8/31 16:07
+ * @Version 1.0.0
+*/
+public class ToolBoxException extends RuntimeException{
+	private static final long serialVersionUID = 8247610319171014183L;
+
+	public ToolBoxException(Throwable e) {
+		super(e.getMessage(), e);
+	}
+	
+	public ToolBoxException(String message) {
+		super(message);
+	}
+	
+	public ToolBoxException(String messageTemplate, Object... params) {
+		super(StrKit.format(messageTemplate, params));
+	}
+	
+	public ToolBoxException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+	
+	public ToolBoxException(Throwable throwable, String messageTemplate, Object... params) {
+		super(StrKit.format(messageTemplate, params), throwable);
+	}
+}
