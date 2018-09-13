76af144bec407509c72b233c3941091e9ff2eb0a
WCNGS <WCNGS@QQ.COM>
2018-09-13 11:56:49 +0800

    修复编译问题

--- uoo-base/uoo-gateway/src/test/java/cn/ffcs/uoo/base/gateway/UooGatewayApplicationTests.java
+++ uoo-base/uoo-gateway/src/test/java/cn/ffcs/uoo/base/gateway/UooGatewayApplicationTests.java
@@ -3,10 +3,10 @@ package cn.ffcs.uoo.base.gateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {UooGatewayApplication.class})
 public class UooGatewayApplicationTests {
 
     @Test
