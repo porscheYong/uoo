59686a98a57ca059034db63abb5049c2d59f7fbb
WCNGS <WCNGS@QQ.COM>
2018-09-19 18:30:17 +0800

    新增"消息队列-队列管理服务"

---
+++ uoo-core/uoo-activiti/activiti-core/src/main/java/cn/ffcs/uoo/App.java
@@ -0,0 +1,35 @@
+package cn.ffcs.uoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  ┏┓　　　┏┓
+ *┏┛┻━━━┛┻┓
+ *┃　　　　　　　┃ 　
+ *┃　　　━　　　┃
+ *┃　┳┛　┗┳　┃
+ *┃　　　　　　　┃
+ *┃　　　┻　　　┃
+ *┃　　　　　　　┃
+ *┗━┓　　　┏━┛
+ *　　┃　　　┃神兽保佑
+ *　　┃　　　┃代码无BUG！
+ *　　┃　　　┗━━━┓
+ *　　┃　　　　　　　┣┓
+ *　　┃　　　　　　　┏┛
+ *　　┗┓┓┏━┳┓┏┛
+ *　　　┃┫┫　┃┫┫
+ *　　　┗┻┛　┗┻┛
+ * @ClassName App
+ * @Description 
+ * @author WCNGS@QQ.COM
+ * @date 2018/9/19 18:14
+ * @Version 1.0.0
+*/
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
--- uoo-core/uoo-data-share/uoo-rabbitmq-manage/src/main/java/cn/ffcs/uoo/rabbitmq/manage/constant/Key.java
+++
@@ -1,5 +0,0 @@
-package cn.ffcs.uoo.rabbitmq.manage.constant;
-
-public class Key {
-	public static final String KEY ="123456";
-}
