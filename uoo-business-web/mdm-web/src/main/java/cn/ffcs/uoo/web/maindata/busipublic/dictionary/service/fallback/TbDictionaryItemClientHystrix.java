fcaf90aa01421b5cab7bfc6d6aeae468e26010d5
zhanglu <zhanglu2@ffcs.cn>
2018-11-13 17:10:50 +0800

    误删代码，重新提交

---
+++ uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/busipublic/dictionary/service/fallback/TbDictionaryItemClientHystrix.java
@@ -0,0 +1,45 @@
+package cn.ffcs.uoo.web.maindata.busipublic.dictionary.service.fallback;
+
+import cn.ffcs.uoo.web.maindata.busipublic.dictionary.dto.TbDictionaryItem;
+import cn.ffcs.uoo.web.maindata.busipublic.dictionary.service.TbDictionaryItemClient;
+import cn.ffcs.uoo.web.maindata.busipublic.vo.ResponseResult;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 字典项客户端断路器
+ * @author zhanglu
+ * @date 2018-11-08
+ */
+@Component
+public class TbDictionaryItemClientHystrix implements TbDictionaryItemClient {
+    @Override
+    public ResponseResult<TbDictionaryItem> updateTbDictionaryItem(TbDictionaryItem tbDictionaryItem) {
+        ResponseResult<TbDictionaryItem> responseResult = new ResponseResult<TbDictionaryItem>();
+        responseResult.setState(ResponseResult.STATE_ERROR);
+        responseResult.setMessage("系统暂时不可用");
+        return responseResult;
+    }
+
+    @Override
+    public ResponseResult<TbDictionaryItem> addTbDictionaryItem(TbDictionaryItem tbDictionaryItem) {
+        ResponseResult<TbDictionaryItem> responseResult = new ResponseResult<TbDictionaryItem>();
+        responseResult.setState(ResponseResult.STATE_ERROR);
+        responseResult.setMessage("系统暂时不可用");
+        return responseResult;
+    }
+
+    @Override
+    public ResponseResult<TbDictionaryItem> removeTbDictionaryItem(Long itemId, Long updateUser) {
+        ResponseResult<TbDictionaryItem> responseResult = new ResponseResult<TbDictionaryItem>();
+        responseResult.setState(ResponseResult.STATE_ERROR);
+        responseResult.setMessage("系统暂时不可用");
+        return responseResult;
+    }
+
+    @Override
+    public List<TbDictionaryItem> queryListByDictionaryName(String dictionaryName) {
+        return null;
+    }
+}
---
+++ uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/busipublic/expando/service/TbSystemtableClient.java
@@ -0,0 +1,24 @@
+package cn.ffcs.uoo.web.maindata.busipublic.expando.service;
+
+import cn.ffcs.uoo.web.maindata.busipublic.expando.dto.TbSystemtable;
+import cn.ffcs.uoo.web.maindata.busipublic.expando.service.fallback.TbExpandovalueClientHystrix;
+import cn.ffcs.uoo.web.maindata.busipublic.expando.service.fallback.TbSystemtableClientHystrix;
+import cn.ffcs.uoo.web.maindata.busipublic.vo.ResponseResult;
+import common.config.PersonnelServiceConfiguration;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "uoo-position",configuration = {PersonnelServiceConfiguration.class},fallback = TbSystemtableClientHystrix.class)
+public interface TbSystemtableClient {
+    @RequestMapping(value = "/tbSystemtable/add", method = RequestMethod.POST)
+    ResponseResult<TbSystemtable> addTbSystemtable(@RequestBody TbSystemtable tbSystemtable);
+
+    @RequestMapping(value = "/tbSystemtable/update", method = RequestMethod.POST)
+    ResponseResult<TbSystemtable> updateTbSystemtable(@RequestBody TbSystemtable tbSystemtable);
+
+    @RequestMapping(value = "/tbSystemtable/del", method = RequestMethod.POST)
+    ResponseResult<TbSystemtable> removeTbSystemtable(@RequestParam("tableId") Long tableId, @RequestParam("updateUser") Long updateUser);
+}
