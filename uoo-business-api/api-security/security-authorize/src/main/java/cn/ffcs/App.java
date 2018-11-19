2bdb95e3b61962b6be160fe5cd79a415f30e0f84
WCNGS <WCNGS@QQ.COM>
2018-11-19 15:18:34 +0800

    添加鉴权、认证和修改README描述

---
+++ uoo-business-api/api-security/security-authorize/src/main/java/cn/ffcs/App.java
@@ -0,0 +1,13 @@
+package cn.ffcs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
