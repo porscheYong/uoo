c87bfe0d33b72e0c89ae9f273d90b9806b8d50e1
ffcs-gzb <guozhb@ffcs.cn>
2018-11-24 15:09:14 +0800

    gzb

--- uoo-business/business-organization/src/main/java/cn/ffcs/uoo/core/organization/controller/OrgTreeController.java
+++ uoo-business/business-organization/src/main/java/cn/ffcs/uoo/core/organization/controller/OrgTreeController.java
@@ -348,13 +348,11 @@ public class OrgTreeController extends BaseController {
     @UooLog(value = "查询组织树列表",key = "getOrgTreeList")
     @RequestMapping(value = "/getOrgTreeList",method = RequestMethod.GET)
     @Transactional(rollbackFor = Exception.class)
-    public ResponseResult<List<OrgTree>>  getOrgTreeList(OrgTree orgTree){
+    public ResponseResult<List<OrgTree>>  getOrgTreeList(String orgTreeId,String orgRootId){
         ResponseResult<List<OrgTree>> ret = new ResponseResult<List<OrgTree>>();
         Wrapper orgTreeWrapper = Condition.create().eq("STATUS_CD","1000").orderBy("SORT");
-        if(orgTree != null){
-            if(!StrUtil.isNullOrEmpty(orgTree.getOrgId())){
-                orgTreeWrapper.eq("ORG_ID",orgTree.getOrgId());
-            }
+        if(!StrUtil.isNullOrEmpty(orgTreeId)){
+            orgTreeWrapper.eq("ORG_TREE_ID",orgTreeId);
         }
         List<OrgTree> orgTreeList = orgTreeService.selectList(orgTreeWrapper);
         ret.setData(orgTreeList);
