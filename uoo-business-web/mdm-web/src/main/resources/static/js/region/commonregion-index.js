69a6842680a04389389c1281d0e0514bb5afb8f2
zengxsh <zengxsh@ffcs.cn>
2018-12-05 10:31:13 +0800

    树搜索

--- uoo-business-web/mdm-web/src/main/resources/static/js/region/commonregion-index.js
+++ uoo-business-web/mdm-web/src/main/resources/static/js/region/commonregion-index.js
@@ -9,7 +9,8 @@ var setting = {
 	view : {
 		showLine : false,
 		showIcon : false,
-		dblClickExpand : false
+		dblClickExpand : false,
+		fontCss: getFontCss
 	},
 	callback : {
 		onClick : zTreeOnClick
@@ -38,6 +39,12 @@ $(document).ready(function() {
 	/*$("#asyncAllBtn").bind("click", asyncAll);
 	$("#resetBtn").bind("click", reset);*/
 });
+function getFontCss(treeId, treeNode) {
+	return (!!treeNode.highlight) ? {color:"#A60000", "font-weight":"bold"} : {color:"#333", "font-weight":"normal"};
+}
+function getAllNodesFilter(node){
+	return true;
+}
 function searchNotWantFilter(node){
 	 return ( node.name.indexOf($('#searchInput').val())<0);
 }
@@ -45,22 +52,44 @@ function searchWantFilter(node){
 	return ( node.name.indexOf($('#searchInput').val())>=0);
 }
 function searchLeftTree(){
-	/*var key=$('#searchInput').val();
-	if(key.length<1){
-		return;
-	}*/
+	var key=$('#searchInput').val();
 	var zTree = $.fn.zTree.getZTreeObj("standardTree");
-	var notWantNodes=zTree.getNodesByFilter(searchNotWantFilter);
+	var nodes = zTree.getNodesByFilter(getAllNodesFilter);
+	if(key.length<1){
+		for(var i=0;i<nodes.length;i++){
+			nodes[i].highlight=false;
+			//zTree.showNode(nodes[i]);
+			zTree.updateNode(nodes[i]);
+		}
+		return;
+	}
+	//先把所有隐藏，再挨个显示
+	for(var i=0;i<nodes.length;i++){
+		nodes[i].highlight=false;
+		//zTree.hideNode(nodes[i]);
+		zTree.updateNode(nodes[i]);
+	}
+	
+	
+	
+	//var notWantNodes=zTree.getNodesByFilter(searchNotWantFilter);
 	var wantNodes=zTree.getNodesByFilter(searchWantFilter);
 	for(var i=0;i<wantNodes.length;i++){
-		wantNodes[i].isHidden=false;
+		wantNodes[i].highlight=true;
 		zTree.updateNode(wantNodes[i]);
 	}
-	for(var i=0;i<notWantNodes.length;i++){
-		notWantNodes[i].isHidden=true;
-		zTree.updateNode(notWantNodes[i]);
+	//收紧所有
+	zTree.expandAll(false);
+	//先把扩展打开  再显示
+	for(var i=0;i<wantNodes.length;i++){
+		var myPN=wantNodes[i].getParentNode();
+		while(myPN!=null){
+			//zTree.showNode(myPN);
+			zTree.expandNode(myPN,true);
+			myPN=myPN.getParentNode();
+		}
+		//zTree.showNode(wantNodes[i]);
 	}
-	console.log('notWantNodes:'+notWantNodes.length+",wantNodes:"+wantNodes.length);
 }
 function loadTypeArr(){
 	$.ajax({
