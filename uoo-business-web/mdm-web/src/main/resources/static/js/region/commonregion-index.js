b7487af1a5e500998124065188d385634599ed76
zengxsh <zengxsh@ffcs.cn>
2018-12-10 15:51:02 +0800

    导航条增加跳转功能

--- uoo-business-web/mdm-web/src/main/resources/static/js/region/commonregion-index.js
+++ uoo-business-web/mdm-web/src/main/resources/static/js/region/commonregion-index.js
@@ -132,7 +132,14 @@ function getCurrentSelectedNode() {
 	var nodes = treeObj.getSelectedNodes();
 	return nodes;
 }
-   
+function selectNodeById(id){
+	console.log(id);
+	var zTree = $.fn.zTree.getZTreeObj("standardTree");
+	var node=zTree.getNodesByParam('id',id);
+	console.log(node[0]);
+	zTree.selectNode(node[0]);
+	zTreeOnClick(null,null,node[0])
+}
 function changeIframe(url) {
 	$('#myFrame').attr("src", url);
 }
--- uoo-business-web/mdm-web/src/main/resources/static/js/region/polloc-list.js
+++ uoo-business-web/mdm-web/src/main/resources/static/js/region/polloc-list.js
@@ -10,6 +10,9 @@ function goEdit(){
 	}
 	parent.changeIframe('/inaction/region/polloc-edit.html?id='+cid+'&parentLocId='+pid+'&parentLocName='+pname);
 }
+function fastGo(id){
+	parent.selectNodeById(id);
+}
 function getRegionList (id) {
 	//初始化页面显示
 	var nodes= parent.getCurrentSelectedNode();
@@ -18,10 +21,10 @@ function getRegionList (id) {
 	var str=treeNode.name;
 	var parentNode=treeNode.getParentNode();
 	while(parentNode!=null){
-		str=parentNode.name+">"+str;
+		str="<a href='javascript:void(0)' onclick=\"fastGo('"+parentNode.id+"')\">"+parentNode.name+"</a> >"+str;
 		parentNode=parentNode.getParentNode();
 	}
-	$('#regionStrFull').text(str);
+	$('#regionStrFull').html(str);
 	$('#regionStrCurrent').attr('cid',nodes[0].id);
 	$.ajax({
 		url:'/region/politicalLocation/getChildPoliticalLocationInfo/'+id,
