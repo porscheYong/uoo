42cc9083644367f513fb717add0185b1908dd78f
wudj <woodj41@163.com>
2018-12-10 10:07:19 +0800

    zipkin配置，mq消息迁出common

--- uoo-business/business-personnel/src/main/java/cn/ffcs/uoo/core/personnel/aop/RabbitMqPsnAop.java
+++ uoo-business/business-personnel/src/main/java/cn/ffcs/uoo/core/personnel/aop/RabbitMqPsnAop.java
@@ -2,9 +2,10 @@ package cn.ffcs.uoo.core.personnel.aop;
 
 import cn.ffcs.uoo.core.personnel.annotion.SendMqMsg;
 
+import cn.ffcs.uoo.core.personnel.constant.BaseUnitConstants;
 import cn.ffcs.uoo.core.personnel.vo.MqContextVo;
 import cn.ffcs.uoo.core.personnel.vo.MqMessageVo;
-import cn.ffcs.uoo.core.personnel.vo.ResponseResultVo;
+import cn.ffcs.uoo.base.common.vo.ResponseResultVo;
 import com.alibaba.fastjson.JSON;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -15,16 +16,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
-/**
- * @ClassName RabbitMqAop
- * @Description MQ调用
- * @author WCNGS@QQ.COM
- * @date 2018/8/29 22:25
- * @Version 1.0.0
- */
 @Aspect
 @Component
-public class RabbitMqAop {
+public class RabbitMqPsnAop {
 
     @Resource
     private AmqpTemplate template;
@@ -38,7 +32,7 @@ public class RabbitMqAop {
 
 
         ResponseResultVo responseResult = (ResponseResultVo)result;
-        if("1000".equals(String.valueOf(responseResult.getState())) &&
+        if(BaseUnitConstants.ENTT_STATE_ACTIVE.equals(String.valueOf(responseResult.getState())) &&
                 responseResult.getData() != null){
             MqMessageVo mqMessageVo = new MqMessageVo();
             MqContextVo mqContextVo = new MqContextVo();
@@ -51,8 +45,7 @@ public class RabbitMqAop {
 
             mqMessageVo.setContext(mqContextVo);
 
-            System.out.println("aa:" + JSON.toJSONString(mqMessageVo));
-            template.convertAndSend("message_sharing_center_queue", JSON.toJSONString(mqMessageVo));
+            template.convertAndSend(BaseUnitConstants.MSG_SHARING_QUEUE, JSON.toJSONString(mqMessageVo));
 
         }
         return result;
