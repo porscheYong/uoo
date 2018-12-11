ea2e6c7d5565ebf8fcf075840d7675e86731c58c
zengxsh <zengxsh@ffcs.cn>
2018-12-11 15:46:14 +0800

    xiugaimima

--- uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/sysuser/client/fallback/SysUserClientHystrix.java
+++ uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/sysuser/client/fallback/SysUserClientHystrix.java
@@ -3,6 +3,7 @@ package cn.ffcs.uoo.web.maindata.sysuser.client.fallback;
 import org.springframework.stereotype.Component;
 
 import cn.ffcs.uoo.web.maindata.sysuser.client.SysUserClient;
+import cn.ffcs.uoo.web.maindata.sysuser.dto.AlterPwdDTO;
 import cn.ffcs.uoo.web.maindata.sysuser.dto.SysUser;
 import cn.ffcs.uoo.web.maindata.sysuser.vo.ResponseResult;
 @Component
@@ -19,6 +20,14 @@ public class SysUserClientHystrix implements SysUserClient {
     @Override
     public ResponseResult<SysUser> getSysUserByAccout(SysUser sysUser) {
         ResponseResult<SysUser> rr=new ResponseResult<>();
+        rr.setState(ResponseResult.STATE_SERVICE_ERROR);
+        rr.setMessage("服务不可用");
+        return rr;
+    }
+
+    @Override
+    public ResponseResult<String> alterPwd(AlterPwdDTO alterPwdDTO) {
+        ResponseResult<String> rr=new ResponseResult<>();
         rr.setState(ResponseResult.STATE_SERVICE_ERROR);
         rr.setMessage("服务不可用");
         return rr;
