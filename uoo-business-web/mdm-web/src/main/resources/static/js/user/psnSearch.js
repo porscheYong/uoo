f1a8715eafcd1280e3d5ea833d8d40daeae06ede
huangjunhao <huangjunhao@ffcs.cn>
2018-12-12 13:52:34 +0800

    1、排序 2、登出 3、修改密码

--- uoo-business-web/mdm-web/src/main/resources/static/js/user/psnSearch.js
+++ uoo-business-web/mdm-web/src/main/resources/static/js/user/psnSearch.js
@@ -21,83 +21,9 @@ engine = new Bloodhound({
     }
 });
 
-function initPerTable(search) {
-    table.destroy();
-    table = $("#personnelTable").DataTable({
-        'searching': false,
-        'autoWidth': false,
-        'ordering': true,
-        "scrollY": "375px",
-        'columns': [
-            { 'data': null, 'title': '序号', 'className': 'row-no' },
-            { 'data': "psnName", 'title': '姓名', 'className': 'row-name',
-                'render': function (data, type, row, meta) {
-                    return "<a href='edit.html?id=" + row.orgId + "&orgRootId=" + row.orgRootId + "&personnelId=" + row.personnelId + "&orgTreeId="+orgTreeId+"'>" + row.psnName + "</a>";
-                }
-            },
-            { 'data': "doubleName", 'title': '重名称谓', 'className': 'row-mobile' },
-            { 'data': "certNo", 'title': '员工工号', 'className': 'cert-no' },
-            { 'data': "postName", 'title': '职位名称', 'className': 'post-name' },
-            { 'data': "orgName", 'title': '所属组织', 'className': 'org-name' },
-            { 'data': "statusCd", 'title': '状态', 'className': 'status-code' }
-        ],
-        'language': {
-            'emptyTable': '没有数据',  
-            'loadingRecords': '加载中...',  
-            'processing': '查询中...',  
-            'search': '检索:',  
-            'lengthMenu': ' _MENU_ ',  
-            'zeroRecords': '没有数据',  
-            'paginate': {  
-                'first':      '首页',  
-                'last':       '尾页',  
-                'next':       '下一页',  
-                'previous':   '上一页'  
-            },  
-            'info': '总_TOTAL_人',  
-            'infoEmpty': '没有数据'
-        },
-        "aLengthMenu": [[10, 20, 50], ["10条/页", "20条/页", "50条/页"]],
-        'pagingType': 'simple_numbers',
-        'dom': '<"top"f>t<"bottom"ipl>',
-        'drawCallback': function(){
-            this.api().column(0).nodes().each(function(cell, i) {
-                cell.innerHTML =  i + 1;
-            });
-        },
-        'serverSide': true,  //启用服务器端分页
-        'ajax': function (data, callback, settings) {
-            var param = {};
-            param.pageSize = data.length;//页面显示记录条数，在页面显示每页显示多少项的时候
-            param.pageNo = (data.start / data.length) + 1;//当前页码
-            param.orgTreeId = orgTreeId;
-            param.orgId = orgId;
-            param.search = search;
-            $http.get('/orgPersonRel/getPerOrgRelPage', param, function (result) {
-                var returnData = {};
-                // returnData.draw = data.draw;//这里直接自行返回了draw计数器,应该由后台返回
-                returnData.recordsTotal = result.total;//返回数据全部记录
-                returnData.recordsFiltered = result.total;//后台不实现过滤功能，每次查询均视作全部结果
-                returnData.data = result.records;//返回的数据列表
-                //调用DataTables提供的callback方法，代表数据已封装完成并传回DataTables进行渲染
-                //此时的数据需确保正确无误，异常判断应在执行此回调前自行处理完毕
-                callback(returnData);
-            }, function (err) {
-
-            })
-        }
-    });
-    var loading = parent.loading;
-    loading.screenMaskDisable('container');
-}
-
-
-
 function engineWithDefaults(q, sync, async) {
     if (q === '') {
-        $('#personnelTable').html('');
-        table.destroy();
-        initOrgPersonnelTable();
+        sortFlag = 0;
     }
     else {
         engine.search(q, sync, async);
@@ -125,7 +51,12 @@ $('#psnName').typeahead({
 })
   .on('typeahead:asyncrequest', function() {
         $('.Typeahead-spinner').show();
-        initPerTable($("#psnName").val());
+        sortFlag = 0;
+        if($("#psnName").val() != ''){
+            initOrgPersonnelTable(checked,$("#psnName").val());
+        }else{
+            initOrgPersonnelTable(checked,'');
+        }
     })
   .on('typeahead:asynccancel', function() {
         $('.Typeahead-spinner').hide();
