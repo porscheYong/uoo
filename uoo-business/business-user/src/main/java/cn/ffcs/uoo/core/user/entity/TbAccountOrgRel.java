442ab6fc5a56afe2f4db6c7a751e766e38f7d44a
wudj <woodj41@163.com>
2018-12-19 17:23:02 +0800

    账号管理

--- uoo-business/business-user/src/main/java/cn/ffcs/uoo/core/user/entity/TbAccountOrgRel.java
+++ uoo-business/business-user/src/main/java/cn/ffcs/uoo/core/user/entity/TbAccountOrgRel.java
@@ -7,17 +7,19 @@ import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.FieldFill;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 
 import java.io.Serializable;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author wudj
  * @since 2018-11-08
  */
+@Data
 @TableName("TB_ACCOUNT_ORG_REL")
 public class TbAccountOrgRel extends Model<TbAccountOrgRel> {
 
@@ -26,8 +28,8 @@ public class TbAccountOrgRel extends Model<TbAccountOrgRel> {
     /**
      * 主账号组织关系标识
      */
-    @TableId("ACCT_HOST_ID")
-    private Long acctHostId;
+    @TableId("ACCT_ORG_REL_ID")
+    private Long acctOrgRelId;
     /**
      * 组织标识
      */
@@ -80,87 +82,6 @@ public class TbAccountOrgRel extends Model<TbAccountOrgRel> {
     @TableField(value = "STATUS_DATE", fill = FieldFill.INSERT)
     private Date statusDate;
 
-
-    public Long getAcctHostId() {
-        return acctHostId;
-    }
-
-    public void setAcctHostId(Long acctHostId) {
-        this.acctHostId = acctHostId;
-    }
-
-    public Long getOrgId() {
-        return orgId;
-    }
-
-    public void setOrgId(Long orgId) {
-        this.orgId = orgId;
-    }
-
-    public Long getAcctId() {
-        return acctId;
-    }
-
-    public void setAcctId(Long acctId) {
-        this.acctId = acctId;
-    }
-
-    public Long getSort() {
-        return sort;
-    }
-
-    public void setSort(Long sort) {
-        this.sort = sort;
-    }
-
-    public String getStatusCd() {
-        return statusCd;
-    }
-
-    public void setStatusCd(String statusCd) {
-        this.statusCd = statusCd;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Long getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(Long createUser) {
-        this.createUser = createUser;
-    }
-
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public Long getUpdateUser() {
-        return updateUser;
-    }
-
-    public void setUpdateUser(Long updateUser) {
-        this.updateUser = updateUser;
-    }
-
-    public Date getStatusDate() {
-        return statusDate;
-    }
-
-    public void setStatusDate(Date statusDate) {
-        this.statusDate = statusDate;
-    }
-
     @Override
     protected Serializable pkVal() {
         return null;
@@ -169,16 +90,16 @@ public class TbAccountOrgRel extends Model<TbAccountOrgRel> {
     @Override
     public String toString() {
         return "TbAccountOrgRel{" +
-        ", acctHostId=" + acctHostId +
-        ", orgId=" + orgId +
-        ", acctId=" + acctId +
-        ", sort=" + sort +
-        ", statusCd=" + statusCd +
-        ", createDate=" + createDate +
-        ", createUser=" + createUser +
-        ", updateDate=" + updateDate +
-        ", updateUser=" + updateUser +
-        ", statusDate=" + statusDate +
-        "}";
+                ", acctOrgRelId=" + acctOrgRelId +
+                ", orgId=" + orgId +
+                ", acctId=" + acctId +
+                ", sort=" + sort +
+                ", statusCd=" + statusCd +
+                ", createDate=" + createDate +
+                ", createUser=" + createUser +
+                ", updateDate=" + updateDate +
+                ", updateUser=" + updateUser +
+                ", statusDate=" + statusDate +
+                "}";
     }
 }
