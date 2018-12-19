e8213f8038d6f23bcff4faa7b9798449a4d1ab84
yanxy <569287549@qq.com>
2018-12-19 14:05:19 +0800

    报错代码注释

--- uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/personnel/service/PersonnelImageService.java
+++ uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/personnel/service/PersonnelImageService.java
@@ -1,17 +1,17 @@
 package cn.ffcs.uoo.web.maindata.personnel.service;
 
-import cn.ffcs.uoo.web.maindata.personnel.service.fallback.EduServiceHystrix;
+//import cn.ffcs.uoo.web.maindata.personnel.service.fallback.EduServiceHystrix;
 import cn.ffcs.uoo.web.maindata.personnel.service.fallback.PersonnelImageHystrix;
-import common.config.PersonnelServiceConfiguration;
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+//import common.config.PersonnelServiceConfiguration;
+//import feign.codec.Encoder;
+//import feign.form.spring.SpringFormEncoder;
+//import org.springframework.beans.factory.ObjectFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.cloud.netflix.feign.support.SpringEncoder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.cloud.netflix.feign.support.SpringEncoder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
