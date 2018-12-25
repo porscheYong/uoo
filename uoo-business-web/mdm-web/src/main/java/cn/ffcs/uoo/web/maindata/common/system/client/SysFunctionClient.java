e776bc33370d4724f27c50cd5bdd028a16d0352b
zengxsh <zengxsh@ffcs.cn>
2018-12-25 16:09:17 +0800

    修复bug

--- uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/common/system/client/SysFunctionClient.java
+++ uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/common/system/client/SysFunctionClient.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.feign.FeignClientProperties.FeignClient
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import cn.ffcs.uoo.web.maindata.common.system.client.fallback.SysFunctionClientHystrix;
 import cn.ffcs.uoo.web.maindata.common.system.dto.SysFunction;
@@ -16,7 +17,7 @@ import cn.ffcs.uoo.web.maindata.common.system.vo.ResponseResult;
 public interface SysFunctionClient {
     
     @GetMapping("/system/sysFunction/list")
-    public ResponseResult<List<SysFunction>> list(Integer pageNo,Integer pageSize,String keyWord);
+    public ResponseResult<List<SysFunction>> list(@RequestParam("pageNo")Integer pageNo,@RequestParam("pageSize")Integer pageSize,@RequestParam("keyWord")String keyWord);
     @RequestMapping(value = "/system/sysFunction/getFunctionByAccout", method = RequestMethod.GET)
     public ResponseResult<List<SysFunction>> getFunctionByAccout(String accout);
 }
