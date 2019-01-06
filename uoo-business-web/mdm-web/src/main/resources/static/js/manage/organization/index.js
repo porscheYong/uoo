e4cd7ee195009a7158e91122fcfd53253e660c9d
yanxy <569287549@qq.com>
2019-01-07 04:20:36 +0800

    1、面包屑点击bug修改 2、平台组织平台用户

--- uoo-business-web/mdm-web/src/main/resources/static/js/manage/organization/index.js
+++ uoo-business-web/mdm-web/src/main/resources/static/js/manage/organization/index.js
@@ -2,34 +2,6 @@
 var loading = new Loading();
 loading.screenMaskEnable('container');
 
-var setting = {
-    async: {
-        enable: true,
-        url: "/orgRel/getOrgRelTree?orgRootId=1&orgTreeId=1",
-        autoParam: ["id"],
-        type: "get",
-        dataFilter: filter
-    },
-    view: {
-        showLine: false,
-        showIcon: false,
-        dblClickExpand: false
-    },
-    data: {
-        key: {
-            isParent: "parent",
-        },
-        simpleData: {
-            enable:true,
-            idKey: "id",
-            pIdKey: "pid",
-            rootPId: ""
-        }
-    },
-    callback: {
-        onClick: onNodeClick
-    }
-};
 var orgId,
     pid,
     orgName,
@@ -37,8 +9,6 @@ var orgId,
     nodeArr;
 
 function onNodeClick(e,treeId, treeNode) {
-    // var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-    // zTree.expandNode(treeNode);
     orgId = treeNode.id;
     pid = treeNode.pid;
     orgName = treeNode.name;
@@ -75,7 +45,7 @@ function getOrgExtInfo () {
                 pathStr += '<span class="breadcrumb-item"><a href="javascript:void(0);" onclick="parent.openTreeById('+orgId+','+node.id+')">' + node.name + '</a><span class="breadcrumb-separator" style="margin: 0 9px;">/</span></span>';
             }
         }
-        $('#orgFrame').contents().find('.breadcrumb').html(pathStr);
+        $('#platformOrgFrame').contents().find('.breadcrumb').html(pathStr);
     }
 }
 
@@ -86,19 +56,47 @@ function filter (treeId, parentNode, childNodes) {
 
 function refreshResult () {
     var url = "list.html?id=" + orgId + '&pid=' + pid + "&name=" + encodeURI(orgName);
-    $('#orgFrame').attr("src",url);
+    $('#platformOrgFrame').attr("src",url);
 }
 
 function initOrgRelTree () {
+    var setting = {
+        async: {
+            enable: true,
+            url: '/orgRel/getOrgRelTree?orgRootId=1&orgTreeId=1',
+            autoParam: ['id'],
+            type: 'get',
+            dataFilter: filter
+        },
+        view: {
+            showLine: false,
+            showIcon: false,
+            dblClickExpand: false
+        },
+        data: {
+            key: {
+                isParent: 'parent'
+            },
+            simpleData: {
+                enable:true,
+                idKey: 'id',
+                pIdKey: 'pid',
+                rootPId: ''
+            }
+        },
+        callback: {
+            onClick: onNodeClick
+        }
+    };
     $http.get('/orgRel/getOrgRelTree', {
         orgRootId: '1',
         orgTreeId: '1'
     }, function (data) {
-        $.fn.zTree.init($("#standardTree"), setting, data);
-        var zTree = $.fn.zTree.getZTreeObj("standardTree");
-        var nodes = zTree.getNodes();
-        zTree.expandNode(nodes[0], true);
-        zTree.selectNode(nodes[0], true);
+        $.fn.zTree.init($("#platformOrgTree"), setting, data);
+        var tree = $.fn.zTree.getZTreeObj("platformOrgTree");
+        var nodes = tree.getNodes();
+        tree.expandNode(nodes[0], true);
+        tree.selectNode(nodes[0], true);
         onNodeClick(null, null, nodes[0]);
     }, function (err) {
 
@@ -107,9 +105,9 @@ function initOrgRelTree () {
 
 // 根据组织ID展开并选中组织
 function openTreeById (sId, id) {
-    var tId = 'standardTree_' + id;
-    var sId = 'standardTree_' + sId;
-    var zTree = $.fn.zTree.getZTreeObj("standardTree");
+    var tId = 'platformOrgTree_' + id;
+    var sId = 'platformOrgTree_' + sId;
+    var zTree = $.fn.zTree.getZTreeObj("platformOrgTree");
     var node = zTree.getNodeByTId(tId);
     zTree.selectNode(node);
     $('.curSelectedNode').trigger('click');
@@ -138,15 +136,6 @@ function deleteNode(orgId) {
     var zTree = $.fn.zTree.getZTreeObj("standardTree");
     var selectNode = zTree.getNodeByTId(tId); //获取当前选中的节点并取消选择状态
     zTree.removeNode(selectNode);
-}
-
-// 选择根节点
-function selectRootNode () {
-    var rootId = 'standardTree_1';
-    var zTree = $.fn.zTree.getZTreeObj("standardTree");
-    var rootNode = zTree.getNodeByTId(rootId);
-    zTree.selectNode(rootNode);
-    $('.curSelectedNode').trigger('click');
 }
 
 initOrgRelTree();
