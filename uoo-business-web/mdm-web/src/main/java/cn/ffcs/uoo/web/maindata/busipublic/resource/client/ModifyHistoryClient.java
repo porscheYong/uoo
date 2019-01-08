927cf2ba297ca9e1caef0f6741ebf1ec033d00ee
zengxsh <zengxsh@ffcs.cn>
2019-01-08 16:53:47 +0800

    分页更新记录

---
+++ uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/busipublic/resource/client/ModifyHistoryClient.java
@@ -0,0 +1,20 @@
+package cn.ffcs.uoo.web.maindata.busipublic.resource.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.feign.FeignClientProperties.FeignClientConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.baomidou.mybatisplus.plugins.Page;
+
+import cn.ffcs.uoo.web.maindata.busipublic.resource.client.fallback.ModifyHistoryClientHystrix;
+import cn.ffcs.uoo.web.maindata.busipublic.resource.dto.ModifyHistory;
+import cn.ffcs.uoo.web.maindata.busipublic.vo.ResponseResult;
+
+@FeignClient(value = "business-public",configuration = {FeignClientConfiguration.class},fallback = ModifyHistoryClientHystrix.class)
+public interface ModifyHistoryClient {
+    
+    @GetMapping("/public/modifyHistory/listByRecord")
+    public ResponseResult<Page<ModifyHistory>> listByRecord(@RequestParam(value = "pageNo") Integer pageNo, @RequestParam(value = "pageSize") Integer pageSize,
+            @RequestParam(value="tableName")String tableName,@RequestParam(value="recordId")String recordId);
+}
