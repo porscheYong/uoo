412a686cb1b2ee8cb1ea9739b9c4ed414488fa35
wuhao <1125636094@qq.com>
2019-01-09 15:52:10 +0800

    添加第三方交互的代码。

--- uoo-psb/psb-cpc/src/main/java/cn/ffcs/interfaces/cpc/service/impl/CpcChannelServiceImpl.java
+++ uoo-psb/psb-cpc/src/main/java/cn/ffcs/interfaces/cpc/service/impl/CpcChannelServiceImpl.java
@@ -1,12 +1,38 @@
 package cn.ffcs.interfaces.cpc.service.impl;
 
 import cn.ffcs.interfaces.cpc.service.CpcChannelService;
+import cn.ffcs.interfaces.cpc.util.Json2MapUtil;
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLOutput;
+import java.util.Map;
 
 @Service
 public class CpcChannelServiceImpl implements CpcChannelService {
     @Override
     public String handle(String json) {
+        Map<String,Object> map = Json2MapUtil.handle(json);
+
+
         return null;
+    }
+
+    public static void main(String[] args) {
+        String json = "{\"TransactionID\":\"1000000045201901078888457736\",\"CHANNEL\":{\"CHANNEL_NBR\":" +
+                "\"3301063199917\",\"CHANNEL_NAME\":\"西湖梦途丰谭路专柜\",\"CHANNEL_CLASS\":\"10\"," +
+                "\"CHN_TYPE_CD\":\"100202\",\"CHANNEL_TYPE_CD\":\"100000\",\"COMMON_REGION_ID\":\"8330106\"," +
+                "\"STATUS_CD\":\"1000\",\"STATUS_DATE\":\"20190109151622\",\"DESCRIPTION\":null,\"ACTION\":" +
+                "\"MOD\"},\"OPERATORS\":{\"OPERATORS_NBR\":\"J33010757714\",\"OPERATORS_NAME\":\"余婷\"," +
+                "\"OPERATORS_TYPE_CD\":\"20\",\"OPERATORS_AREA_GRADE\":\"1000\",\"PARENT_OPER_NBR\":null," +
+                "\"COMMON_REGION_ID\":\"8330106\",\"STATUS_CD\":\"1000\",\"STATUS_DATE\":null,\"DESCRIPTION\":null," +
+                "\"ACTION\":\"MOD\"},\"STAFF\":{\"SALES_CODE\":\"Y99999999\",\"STAFF_CODE\":\"Y99999999\",\"ACCOUNT\":" +
+                "\"999999\",\"STAFF_NAME\":\"测试yxg123\",\"CERT_TYPE\":\"1\",\"CERT_NUMBER\":\"330702197911041225\"," +
+                "\"MOBILE_PHONE\":\"15372128883\",\"E_MAIL\":null,\"COMMON_REGION_ID\":\"8330106\",\"STAFF_DESC\":null," +
+                "\"STATUS_CD\":\"1000\",\"STATUS_DATE\":null,\"CREATE_DATE\":null,\"ACTION\":\"MOD\"}," +
+                "\"CHANNEL_OPERATORS_RELAS\":{\"CHANNEL_NBR\":\"3301063199917\",\"OPERATORS_NBR\":\"J33010757714\"," +
+                "\"RELA_TYPE\":\"10\",\"DESCRIPTION\":null,\"ACTION\":\"MOD\"},\"STAFF_CHANNEL_RELAS\":{\"SALES_CODE\":" +
+                "\"Y99999999\",\"CHANNEL_NBR\":\"3301063199917\",\"RELA_TYPE\":\"10\",\"DESCRIPTION\":null,\"ACTION\":\"MOD\"}}";
+        System.out.println(new CpcChannelServiceImpl().handle(json));
     }
 }
