522ba78fdaa49e8e6d4d5ad239516b149d9a032a
zengxsh <zengxsh@ffcs.cn>
2019-01-18 10:05:55 +0800

    修改权限get接口

---
+++ uoo-business-web/mdm-web/src/main/java/cn/ffcs/uoo/web/maindata/common/system/dto/SysFile.java
@@ -0,0 +1,196 @@
+package cn.ffcs.uoo.web.maindata.common.system.dto;
+
+import java.util.Date;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zengxsh
+ * @since 2018-12-24
+ */
+@TableName("SYS_FILE")
+public class SysFile extends Model<SysFile> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId("FILE_ID")
+    private Long fileId;
+    @TableField("FILE_NAME")
+    private String fileName;
+    @TableField("FILE_TYPE")
+    private String fileType;
+    @TableField("FILE_SIZE")
+    private Double fileSize;
+    @TableField("FILE_DESC")
+    private String fileDesc;
+    @TableField("FILE_VERSION")
+    private String fileVersion;
+    @TableField("FILE_ADDR")
+    private String fileAddr;
+    /**
+     * 状态
+     */
+    @TableField("STATUS_CD")
+    private String statusCd;
+    /**
+     * 创建时间
+     */
+    @TableField("CREATE_DATE")
+    private Date createDate;
+    /**
+     * 创建人
+     */
+    @TableField("CREATE_USER")
+    private Long createUser;
+    /**
+     * 修改时间
+     */
+    @TableField("UPDATE_DATE")
+    private Date updateDate;
+    /**
+     * 修改人
+     */
+    @TableField("UPDATE_USER")
+    private Long updateUser;
+    /**
+     * 状态变更的时间
+     */
+    @TableField("STATUS_DATE")
+    private Date statusDate;
+
+
+    public Long getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Long fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public Double getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Double fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileDesc() {
+        return fileDesc;
+    }
+
+    public void setFileDesc(String fileDesc) {
+        this.fileDesc = fileDesc;
+    }
+
+    public String getFileVersion() {
+        return fileVersion;
+    }
+
+    public void setFileVersion(String fileVersion) {
+        this.fileVersion = fileVersion;
+    }
+
+    public String getFileAddr() {
+        return fileAddr;
+    }
+
+    public void setFileAddr(String fileAddr) {
+        this.fileAddr = fileAddr;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Long getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Long createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Long getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Long updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getStatusDate() {
+        return statusDate;
+    }
+
+    public void setStatusDate(Date statusDate) {
+        this.statusDate = statusDate;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.fileId;
+    }
+
+    @Override
+    public String toString() {
+        return "SysFile{" +
+        ", fileId=" + fileId +
+        ", fileName=" + fileName +
+        ", fileType=" + fileType +
+        ", fileSize=" + fileSize +
+        ", fileDesc=" + fileDesc +
+        ", fileVersion=" + fileVersion +
+        ", fileAddr=" + fileAddr +
+        ", statusCd=" + statusCd +
+        ", createDate=" + createDate +
+        ", createUser=" + createUser +
+        ", updateDate=" + updateDate +
+        ", updateUser=" + updateUser +
+        ", statusDate=" + statusDate +
+        "}";
+    }
+}
