94b1ddb0723ab9efb634fcee73a22439b8acd698
WCNGS <WCNGS@QQ.COM>
2019-01-25 11:46:25 +0800

    添加日志API服务查询

---
+++ uoo-base/base-common/common-persistence/persistence-jpa/src/main/java/xyz/wongs/common/exception/SpringHandlerExceptionAdvice.java
@@ -0,0 +1,53 @@
+package xyz.wongs.common.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  ┏┓　　　┏┓
+ *┏┛┻━━━┛┻┓
+ *┃　　　　　　　┃ 　
+ *┃　　　━　　　┃
+ *┃　┳┛　┗┳　┃
+ *┃　　　　　　　┃
+ *┃　　　┻　　　┃
+ *┃　　　　　　　┃
+ *┗━┓　　　┏━┛
+ *　　┃　　　┃神兽保佑
+ *　　┃　　　┃代码无BUG！
+ *　　┃　　　┗━━━┓
+ *　　┃　　　　　　　┣┓
+ *　　┃　　　　　　　┏┛
+ *　　┗┓┓┏━┳┓┏┛
+ *　　　┃┫┫　┃┫┫
+ *　　　┗┻┛　┗┻┛
+ * @ClassName SpringHandlerExceptionAdvice
+ * @Description  增强器
+ * @author WCNGS@QQ.COM
+ * @date 2019/1/25 11:14
+ * @Version 1.0.0
+*/
+@ControllerAdvice
+public class SpringHandlerExceptionAdvice {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {}
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("author", "Magical Sam");
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public Map errorHandler(Exception ex) {
+        Map map = new HashMap();
+        map.put("code", 100);
+        map.put("msg", ex.getMessage());
+        return map;
+    }
+}
