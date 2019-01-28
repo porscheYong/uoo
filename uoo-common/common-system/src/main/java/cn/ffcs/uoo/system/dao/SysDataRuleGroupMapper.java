057f09145f9bed35540d15f553938522a2a2fe77
zengxsh <zengxsh@ffcs.cn>
2019-01-28 15:01:10 +0800

    .

---
+++ uoo-common/common-system/src/main/java/cn/ffcs/uoo/system/dao/SysDataRuleGroupMapper.java
@@ -0,0 +1,15 @@
+package cn.ffcs.uoo.system.dao;
+
+import cn.ffcs.uoo.system.entity.SysDataRuleGroup;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author zengxsh
+ * @since 2019-01-28
+ */
+public interface SysDataRuleGroupMapper extends BaseMapper<SysDataRuleGroup> {
+    Long getId();
+}
--- uoo-common/common-system/src/main/java/cn/ffcs/uoo/system/entity/SysDataRuleGroup.java
+++ uoo-common/common-system/src/main/java/cn/ffcs/uoo/system/entity/SysDataRuleGroup.java
@@ -9,48 +9,40 @@ import java.io.Serializable;
 
 /**
  * <p>
-* 记录权限下相关联的规则，包括横向、纵向的数据维度。
-只有需要权限控制的表才进行登记
  
  * </p>
  *
  * @author zengxsh
  * @since 2019-01-28
  */
-@TableName("SYS_DATA_RULE")
-public class SysDataRule extends Model<SysDataRule> {
+@TableName("SYS_DATA_RULE_GROUP")
+public class SysDataRuleGroup extends Model<SysDataRuleGroup> {
 
     private static final long serialVersionUID = 1L;
 
     /**
-     * 权限规则标识,主键
+     * 权限规则组标识,主键
      */
-    @TableId("DATA_RULE_ID")
-    private Long dataRuleId;
+    @TableId("DATA_RULE_GROUP_ID")
+    private Long dataRuleGroupId;
 
     /**
-     * 表的名称
+     * 组名称
      */
-    @TableField("TAB_NAME")
-    private String tabName;
+    @TableField("GROUP_NAME")
+    private String groupName;
 
     /**
-     * 记录字段名称
+     * 规则操作符,and or
      */
-    @TableField("COL_NAME")
-    private String colName;
+    @TableField("AND_OR")
+    private String andOr;
 
     /**
-     * 规则操作符,包含、等于、大于、大于等于、小于、小于等于
+     * 排序
      */
-    @TableField("RULE_OPERATOR")
-    private String ruleOperator;
-
-    /**
-     * 字段值,同一权限的同一业务对象属性标识的多个取值,可用逗号间隔
-     */
-    @TableField("COL_VALUE")
-    private String colValue;
+    @TableField("SORT")
+    private Double sort;
 
     /**
      * 状态
@@ -88,52 +80,36 @@ public class SysDataRule extends Model<SysDataRule> {
     @TableField("UPDATE_USER")
     private Long updateUser;
 
-    /**
-     * 权限规则组标识
-     */
-    @TableField("DATA_RULE_GROUP_ID")
-    private Long dataRuleGroupId;
+    @TableField("PARENT_RULE_GROUP_ID")
+    private Long parentRuleGroupId;
 
-    /**
-     * 条件操作符
-     */
-    @TableField("AND_OR")
-    private String andOr;
-
-    public Long getDataRuleId() {
-        return dataRuleId;
+    public Long getDataRuleGroupId() {
+        return dataRuleGroupId;
     }
 
-    public void setDataRuleId(Long dataRuleId) {
-        this.dataRuleId = dataRuleId;
+    public void setDataRuleGroupId(Long dataRuleGroupId) {
+        this.dataRuleGroupId = dataRuleGroupId;
     }
-    public String getTabName() {
-        return tabName;
-    }
-
-    public void setTabName(String tabName) {
-        this.tabName = tabName;
-    }
-    public String getColName() {
-        return colName;
+    public String getGroupName() {
+        return groupName;
     }
 
-    public void setColName(String colName) {
-        this.colName = colName;
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
     }
-    public String getRuleOperator() {
-        return ruleOperator;
-    }
-
-    public void setRuleOperator(String ruleOperator) {
-        this.ruleOperator = ruleOperator;
-    }
-    public String getColValue() {
-        return colValue;
+    public String getAndOr() {
+        return andOr;
     }
 
-    public void setColValue(String colValue) {
-        this.colValue = colValue;
+    public void setAndOr(String andOr) {
+        this.andOr = andOr;
+    }
+    public Double getSort() {
+        return sort;
+    }
+
+    public void setSort(Double sort) {
+        this.sort = sort;
     }
     public String getStatusCd() {
         return statusCd;
@@ -177,42 +153,33 @@ public class SysDataRule extends Model<SysDataRule> {
     public void setUpdateUser(Long updateUser) {
         this.updateUser = updateUser;
     }
-    public Long getDataRuleGroupId() {
-        return dataRuleGroupId;
+    public Long getParentRuleGroupId() {
+        return parentRuleGroupId;
     }
 
-    public void setDataRuleGroupId(Long dataRuleGroupId) {
-        this.dataRuleGroupId = dataRuleGroupId;
-    }
-    public String getAndOr() {
-        return andOr;
-    }
-
-    public void setAndOr(String andOr) {
-        this.andOr = andOr;
+    public void setParentRuleGroupId(Long parentRuleGroupId) {
+        this.parentRuleGroupId = parentRuleGroupId;
     }
 
     @Override
     protected Serializable pkVal() {
-        return this.dataRuleId;
+        return this.dataRuleGroupId;
     }
 
     @Override
     public String toString() {
-        return "SysDataRule{" +
-        "dataRuleId=" + dataRuleId +
-        ", tabName=" + tabName +
-        ", colName=" + colName +
-        ", ruleOperator=" + ruleOperator +
-        ", colValue=" + colValue +
+        return "SysDataRuleGroup{" +
+        "dataRuleGroupId=" + dataRuleGroupId +
+        ", groupName=" + groupName +
+        ", andOr=" + andOr +
+        ", sort=" + sort +
         ", statusCd=" + statusCd +
         ", statusDate=" + statusDate +
         ", createDate=" + createDate +
         ", createUser=" + createUser +
         ", updateDate=" + updateDate +
         ", updateUser=" + updateUser +
-        ", dataRuleGroupId=" + dataRuleGroupId +
-        ", andOr=" + andOr +
+        ", parentRuleGroupId=" + parentRuleGroupId +
         "}";
     }
 }
