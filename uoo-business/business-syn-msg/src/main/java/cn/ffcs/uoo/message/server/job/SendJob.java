003a1d8ddc6f758162a3ec58f3c2d7440465654f
wuhao <1125636094@qq.com>
2019-02-12 11:41:39 +0800

    添加处理时间。

--- uoo-business/business-syn-msg/src/main/java/cn/ffcs/uoo/message/server/job/SendJob.java
+++ uoo-business/business-syn-msg/src/main/java/cn/ffcs/uoo/message/server/job/SendJob.java
@@ -66,7 +66,10 @@ public class SendJob {
 
                         switch (type){
                             case "orgId":{
-                                TbOrg org = tbOrgMapper.selectById(value);
+                                TbOrg org = null;
+                                if(value != null) {
+                                    org =tbOrgMapper.selectById(value);
+                                }
                                 if(org == null){
                                     temp.setStatusCd("-2");
                                     temp.setFailReason("组织标识不在数据库");
@@ -90,7 +93,10 @@ public class SendJob {
                                 }
                             };break;
                             case "slaveAcctId":{
-                                TbSlaveAcct tbSlaveAcct = tbSlaveAcctMapper.selectById(value);
+                                TbSlaveAcct tbSlaveAcct = null;
+                                if(value != null){
+                                    tbSlaveAcct = tbSlaveAcctMapper.selectById(value);
+                                }
                                 if(tbSlaveAcct == null){
                                     temp.setStatusCd("-2");
                                     temp.setFailReason("从账号标识不在数据库");
@@ -113,7 +119,10 @@ public class SendJob {
                                 }
                             };break;
                             case "personnelId":{
-                                List<TbPersonnel> tbPersonnelList = tbPersonnelMapper.selectPersonnelAndAcctById(value);
+                                List<TbPersonnel> tbPersonnelList = null;
+                                if(value != null){
+                                    tbPersonnelList = tbPersonnelMapper.selectPersonnelAndAcctById(value);
+                                }
                                 if(tbPersonnelList == null && tbPersonnelList.size() <=0){
                                     temp.setStatusCd("-2");
                                     temp.setFailReason("人员/主账号不在数据库");
